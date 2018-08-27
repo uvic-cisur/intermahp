859cdb47422c754acd1001f5b730e642a90d5f57
Samuel Churchill <meadowschurchill@gmail.com>
2018-08-27 11:35:35 -0700

    Removed leftover code
    Error found upon R restart

--- js-utils.R
+++ js-utils.R
@@ -1,25 +1,3 @@
-# popovers
-# popoverInit <- function() {
-#   tags$head(
-#     tags$script(
-#       "$(document).ready(function(){
-#       $('body').popover({
-#       selector: '[data-toggle=\"popover\"]',
-#       trigger: 'hover'        
-#       });});"
-#     )
-#   )
-# }
-# 
-# popover <- function(content, pos, ...) {
-#   tagList(
-#     singleton(popoverInit()),
-#     tags$a(href = "#", `data-toggle` = "popover", `data-placement` = paste("auto", pos),
-#            `data-original-title` = "", title = "", `data-trigger` = "hover",
-#            `data-html` = "true", `data-content` = content, ...)
-#   )
-# }
-
 popoverInit <- function() {
   tags$head(
     tags$script(
@@ -56,7 +34,7 @@ popoverInit <- function() {
 
 popover <- function(content, pos, ...) {
   tagList(
-    singleton(clickPopoverInit()),
+    singleton(popoverInit()),
     tags$a(href = "#", `data-toggle` = "popover", `data-placement` = paste("auto", pos),
            `data-original-title` = "", title = "", `data-trigger` = "click",
            `data-html` = "true", `data-content` = content, ...)
